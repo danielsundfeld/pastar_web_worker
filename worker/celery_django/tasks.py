4a53f6f8310db790d9dea11da0544782d7400f2c
Daniel Sundfeld <daniel.sundfeld@gmail.com>
2021-06-15 16:00:25 -0300

    Using temp files for sequences and removing

--- worker/celery_django/tasks.py
+++ worker/celery_django/tasks.py
@@ -1,17 +1,20 @@
+import os
+import tempfile
 import subprocess
 from .celeryconfig import app
 
 @app.task(bind=True)
 def alinhar_sequencias(self, sequencias):
 
-    path = '/tmp/sequencias.txt'
-    with open(path, 'w') as file:
+    (fd, sequences_file) = tempfile.mkstemp()
+    with open(sequences_file, 'w') as file:
         for sequencia in sequencias:
             file.write(sequencia)
 
     stdout = subprocess.run(
-        ['msa_pastar', path],
+        ['msa_pastar', sequences_file],
         stdout=subprocess.PIPE, encoding='utf8')
+    os.remove(sequences_file)
 
     result = {
         'task_id': self.request.id,
